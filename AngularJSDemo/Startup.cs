173f66d887972f044bf6ce60d2fadbf7aa2abda3
melih154 <melih154@gmail.com>
2014-08-07 15:07:01 +0100

    Initial Commit

---
+++ AngularJSDemo/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AngularJSDemo.Startup))]
+
+namespace AngularJSDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
